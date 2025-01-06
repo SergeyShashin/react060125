fb32d069d0c793e048f9bdce80dc0d4ea36a4fff
SergeyShashin <s9997910@yandex.ru>
2025-01-06 17:29:19 +0300

    React 060125 lesson1 updated.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
-console.log('Privet WORLD!');
+// console.log('Privet WORLD!');
 
-import { User } from './user.js';
+// import { User } from './user.js';
 
-let l = new User('Luck', 'Skywalker');
+// let l = new User('Luck', 'Skywalker');
 
-console.log(l.sayHi());
+// console.log(l.sayHi());
+
+import React from "react";
+import { createRoot } from 'react-dom/client';
+
+const element = React.createElement(
+  'h1',
+  { className: 'react-privet' },
+  'Привет! Реакт работает.'
+);
+
+const root = createRoot(document.getElementById('root'));
+
+root.render(
+  element
+);
