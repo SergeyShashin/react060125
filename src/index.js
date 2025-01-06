82fc02daddfccf60757bff55c0bc7a1398ea944e
SergeyShashin <s9997910@yandex.ru>
2025-01-06 18:07:18 +0300

    React 060125 lesson1 done.

--- src/index.js
+++ src/index.js
@@ -9,14 +9,20 @@
 import React from "react";
 import { createRoot } from 'react-dom/client';
 
-const element = React.createElement(
-  'h1',
-  { className: 'react-privet' },
-  'Привет! Реакт работает.'
-);
+// const element = React.createElement(
+//   'h1',
+//   { className: 'react-privet' },
+//   'Привет! Реакт работает.'
+// );
+
+// const element = <h1 className="react-privet">Привет! JSX тоже работает!</h1>;
+const messages = ['Привет, друг!', 'Как дела?'];
+
+const Message = props => <p>{props.text}</p>;
+const MessageList = props => props.messages.map(messsage => <Message text={messsage} />);
 
 const root = createRoot(document.getElementById('root'));
 
 root.render(
-  element
+  <MessageList messages={messages} />
 );
