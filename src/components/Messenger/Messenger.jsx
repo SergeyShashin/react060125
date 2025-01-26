4858c1cbdf741a047040130e1331fa347e13b6a3
SergeyShashin <s9997910@yandex.ru>
2025-01-26 14:57:11 +0300

    React 060125. Lesson 4 updated.

--- src/components/Messenger/Messenger.jsx
+++ src/components/Messenger/Messenger.jsx
@@ -12,6 +12,7 @@ import { MessageForm } from "components/MessageForm";
 export class Messenger extends Component {
   constructor(props) {
     super(props);
+    console.log(localStorage.getItem('stateMessenger'));
     this.state = {
       // isVisible: false
       currentChat: null,
@@ -72,6 +73,7 @@ export class Messenger extends Component {
     this.setState({
       chats: Object.assign(this.state.chats, this.updateChat(message))
     });
+    localStorage.setItem('stateMessenger', JSON.stringify(this.state))
   };
 
   /**
