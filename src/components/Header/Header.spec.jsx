6e23c272ecbc1c151a345b8d97eef83157dfe956
SergeyShashin <s9997910@yandex.ru>
2025-02-18 00:26:31 +0300

    React 060125. Jest. Updated.

--- src/components/Header/Header.spec.jsx
+++ src/components/Header/Header.spec.jsx
@@ -1,5 +1,22 @@
+import React, { Component }  from 'react';
+import { Header } from './Header';
+// import renderer from 'react-test-renderer';
+import {render} from 'node_modules\@testing-library';
+
 test('Header should be rendered', () => {
-  let a = 2 + 5;
-  expect(a).toBe(7);
-  expect(a).not.toBeNull();
+  // let a = 2 + 5;
+  // expect(a).toBe(7);
+  // expect(a).not.toBeNull();
+
+  // let tree = renderer
+  //   .create(<Header chatName={'Chat1'} />)
+  //   .toJSON();
+
+  // expect(tree).toMatchSnapshot();
+
+  const {asFragment, getByText} = render(<Header chatName={'Chat1'} />)
+  expect(getByText('Chat1')).toBeInTheDocument()
+  // expect(asFragment()).toMatchInlineSnapshot(`
+  //   <h1>Hello, World!</h1>
+  // `)
 });
